1dc46d5bdd73f33009e84cdb6bc3bd89ac5e708d
horik <codeschoolway@gmail.com>
2020-08-19 23:29:31 +0900

    「SEND」をクリックしたら「TRANSMISSION COMPLETED」にボタン文言を変更する

--- html-kaguya/js/common.js
+++ html-kaguya/js/common.js
@@ -12,3 +12,12 @@ $(function(){
   });
 }());
 // ここまでハンバーガーメニュー
+
+// SENDボタンをクリックしたら「TRANSMISSION COMPLETED」にボタン文言を変更する
+const element = document.getElementById('button-value');
+//console.log(element);
+if (element !== null) {
+  document.getElementById('button-value').onclick = () =>{
+    document.getElementById('button-value').value = "TRANSMISSION COMPLETED";
+  };
+}
